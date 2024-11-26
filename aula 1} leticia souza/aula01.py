009be2dc5a72bbc796ded492b686384ef41a834a
leticia-vfer <leticiasouzaferreira957@gmail.com>
2024-11-26 11:35:37 -0300

    adicionei os arquivos da aula 01

---
+++ aula 1} leticia souza/aula01.py
@@ -0,0 +1,5 @@
+print('olá, meu mundo')
+print('hoje o dia está lindo')
+print('estou com saudade do namorado')
+print('hoje irei sair com minhas amigas para o mc')
+print('mas queria ver meu chuchu, beijinhos, saudade da sua amada')
