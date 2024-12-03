d09e4c205e4be8a4945cbf788c61d8e18a34ed09
leticia-vfer <leticiasouzaferreira957@gmail.com>
2024-12-03 09:50:25 -0300

    aula 2

---
+++ aula 2 letcia souza/aula2.py
@@ -0,0 +1,11 @@
+print ('bom dia')
+
+nome_usuario = 'leticia souza' 
+idade_usuario = 21 # Tipo -> int = integer 
+altura_usuario= 1.58  # tipo - > float = ponto flutuante 
+maior_idade= True # tipo - > bool= boolean  
+
+print  ('seja bem vindo' , nome_usuario)
+print ('sua idade é' , idade_usuario , 'anos')
+print ('sua altura é', altura_usuario, 'metros.') 
+print ('você é maior de idade?', maior_idade)
